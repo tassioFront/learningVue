f0f9bafc810e3afc2685dd5bf77db3b246b531cb
TassioSense <tassio.jordao@sensenova.com.br>
2019-04-16 18:43:04 -0300

    Vendo Computed e Watch

---
+++ template/desafio-03/app.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        valor: 0
+    },
+
+    computed: {
+        resultado() {
+            return (this.valor == 37) ? "Igual" : "Diferente"
+        }
+    },
+
+    watch: {
+        resultado() {setTimeout(() => {
+            this.valor = 0
+            }, 5000);
+        }
+    }
+
+});
