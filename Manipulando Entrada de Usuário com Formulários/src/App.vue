cb67e52b494d0efcb389aa358addfb7291eb4057
TassioSense <tassio.jordao@sensenova.com.br>
2019-04-30 13:05:07 -0300

    Terminando os comandos de formulários

--- Manipulando Entrada de Usuário com Formulários/src/App.vue
+++ Manipulando Entrada de Usuário com Formulários/src/App.vue
@@ -2,7 +2,7 @@
 	<div id="app">
 		<h1>Registrar Reclamação</h1>
 		<div class="conteudo">
-			<form class="painel">
+			<form class="painel" v-if="!enviado">
 				<div class="cabecalho">Formulário</div>
 				<Rotulo nome="E-mail">
 					<input type="text" v-model.lazy.trim="usuario.email">
@@ -43,9 +43,11 @@
 					<Escolha v-model="escolha" />
 				</Rotulo>
 				<hr>
-				<button>Enviar</button>
+				<button @click.prevent="enviar">Enviar</button>
 			</form>
-			<div class="painel">
+
+
+			<div class="painel" v-else>
 				<div class="cabecalho">Resultado</div>
 				<Rotulo nome="E-mail">
 					<span> {{ usuario.email }}</span>
@@ -130,8 +132,14 @@ export default {
 				idade:23
 
 			},
-			escolha: true
+			escolha: true,
+			enviado: false
 				
+		}
+	},
+	methods: {
+		enviar() {
+			this.enviado = true
 		}
 	}
 }
