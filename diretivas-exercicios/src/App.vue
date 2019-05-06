f18e43711e4abeecd881a8f03c52d63ad5896751
Tássio Jordão <tassio72@hotmail.com>
2019-05-05 21:50:51 -0300

    Ajustando multidiretivas

--- diretivas-exercicios/src/App.vue
+++ diretivas-exercicios/src/App.vue
@@ -17,6 +17,11 @@
 
 		<p v-destaque-local:atraso.alternar="cor"> Adiciono uma borda depois de 3 segundos devido o modificador </p>
 		
+		<hr>
+
+		<p v-destaque-local:corFonte.atraso.alternar="{cor1: 'green', cor2: 'red', atraso:'2000', intervalo: 200}"> Alterando cor de fundo com diretiva personalizada que manipula o el </p>
+		<p v-destaque-local:atraso.alternar="{cor1: 'green', atraso:'5000'}"> Adiciono uma borda depois de 3 segundos devido o modificador </p>
+		
 
 	</div>
 </template>
@@ -30,6 +35,7 @@ export default {
 	veja https://br.vuejs.org/v2/guide/custom-directive.html
 	para consulta das diretivas gerenciadas por Vue
 	*/
+
 	//diretiva localmente
 	directives: {'destaque-local': {
 
@@ -48,8 +54,8 @@ export default {
 				let atraso = 0
 				if (binding.modifiers['atraso']) atraso = 3000 //verificando se tem o modifier atraso
 
-				const cor1 = binding.value
-				const cor2 = 'black'
+				const cor1 = binding.value.cor1 //forma de acessar um valor de objeto 
+				const cor2 = binding.value.cor2
 				let corAtual = cor1
 
 				setInterval(() => {
@@ -59,15 +65,15 @@ export default {
 						setTimeout(() => {
 							corAtual = corAtual === cor1 ? cor2 : cor1
 							aplicarCor(corAtual)
-						}, 1000)
+						}, binding.value.intervalo)
 
 					} else {
 
-						aplicarCor(binding.value)
+						aplicarCor(binding.value.cor1)
 
 					}
 
-				}, atraso);
+				}, binding.value.atraso);
 				
 				
 
