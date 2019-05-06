8bf12f9d86e5b904fa23173a56e707e5b6cce00f
TassioSense <tassio.jordao@sensenova.com.br>
2019-05-06 18:20:32 -0300

    Trabalhando com filters

---
+++ Filtros e Mixins/filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+	<div id="app">
+		
+		<h1>Filtros & Mixins</h1>
+		<hr>
+		<p> Pegando uma <strong> string </strong> e tratando com filter: {{ cpf | tratandoCpf }}</p>
+
+		<hr>
+
+		<p> Pegando um <strong> número </strong> e tratando com filter: {{ 42571388827 | tratandoCpf }}</p>
+
+		<hr>
+
+		<p> Usando o filter globalmente : {{ cpf | tratandoCpf | inverter }}</p>
+
+		<hr>
+
+		<p> Input tratado com filter
+			<input type="text" :value='cpf | tratandoCpf'>
+		</p>
+	</div>
+</template>
+
+<script>
+
+/*Aplicando filter localmente
+Para issousa-se filter: {
+	função(valor) {
+		tratamento
+	}
+}
+
+Já na interpolação, usamos um pipe para usar o nome do filter, conforme mostrado acima
+valorInterpolado | filterQueTrataValor | n-FilterQueTraraValor...
+
+cpf | tratandoCpf
+
+*/
+export default {
+	filters: {
+		tratandoCpf(valor) {
+			let arr = [];
+
+			if (typeof(valor) == typeof('string')) {
+				arr = valor.split('');	
+			} else {
+				arr = `${valor}`.split('');
+			}
+		
+			
+			arr.splice(3, 0, ".");
+			arr.splice(7, 0, ".");
+			arr.splice(11, 0, "-");
+			return arr.join('');
+		}
+	},
+	data() {
+		return {
+			cpf: '42571388827'
+			}
+	}
+
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+
+input {
+	font-size: 2rem;
+}
+</style>
