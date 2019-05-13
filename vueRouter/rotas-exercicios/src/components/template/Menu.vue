caae83e83929af6215b8edbfde3d1f568c40b1b6
Tássio Jordão <tassio72@hotmail.com>
2019-05-12 23:04:07 -0300

    trabalhando com router-link

---
+++ vueRouter/rotas-exercicios/src/components/template/Menu.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav class="menu">
+        <router-link to='/'> Inicio </router-link>
+        <router-link to='/usuario'> Usuário </router-link>
+    </nav>
+</template>
+
+<script>
+/*router-link
+usando router-link para criar link entre componentes,
+sem a necessidade de fazer novas requisições ao servidor
+*/
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
