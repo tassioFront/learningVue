b02ad6966e2a5820004731dbadcf23ab7a0c22af
Tássio Jordão <tassio72@hotmail.com>
2019-05-13 00:04:17 -0300

    estilizando router-link 2

--- vueRouter/rotas-exercicios/src/components/usuario/Usuario.vue
+++ vueRouter/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -1,13 +1,22 @@
 <template>
     <div class="usuario">
         <h2>Usuario</h2>
+        <hr>
+        <button sucesso @click="irParaInicio"> Voltar</button>
 
     </div>
 </template>
 
 <script>
 export default {
+    methods: {
+        irParaInicio() {
+            //usando $router.push para alterar de componentes. 
 
+            //this.$router.push('/')  // para fins didáditos
+            this.$router.push( { path:'/'})
+        }
+    }
 }
 </script>
 
